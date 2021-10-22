0905c77157eb953086957520a9a78755c21b336d
Abir Khan <khan.abir@yahoo.com>
2021-10-22 22:40:20 +0600

    Removed surefire reports

--- src/test/java/test/TestCases.java
+++ src/test/java/test/TestCases.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.Test;
 import org.apache.log4j.xml.DOMConfigurator;
 
 import utility.ExcelUtils;
